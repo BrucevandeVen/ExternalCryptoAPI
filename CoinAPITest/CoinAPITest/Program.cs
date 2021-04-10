6881980c36f4208bae96301d7311e21764171021
Bruce van de Ven <430690@student.fontys.nl>
2021-04-10 16:27:14 +0200

    Coin api test toegevoegd

---
+++ CoinAPITest/CoinAPITest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace CoinAPITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string json;
+
+                using (var web = new System.Net.WebClient())
+                {
+                    var url = @"https://api.coindesk.com/v1/bpi/currentprice.json";
+                    json = web.DownloadString(url);
+                }
+
+                dynamic obj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                var time = Convert.ToString(obj.time.updated);
+                var currentPrice = Convert.ToDecimal(obj.bpi.EUR.rate);
+
+                Console.WriteLine(time + " " + currentPrice);
+
+                Thread.Sleep(60000);
+            }
+        }
+    }
+}
