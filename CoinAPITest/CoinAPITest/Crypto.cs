357559b0c59b6b9a5d300d50345df744f304c0d7
Bruce van de Ven <430690@student.fontys.nl>
2021-04-15 00:40:16 +0200

    Coinmarketcap

---
+++ CoinAPITest/CoinAPITest/Crypto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinAPITest
+{
+    class Crypto
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+
+        public override string ToString()
+        {
+            return "id: " + Convert.ToString(id) + " " + "name: " + name + " " + " price: " + Convert.ToString(price);
+        }
+    }
+}
--- CoinAPITest/CoinAPITest/Program.cs
+++ CoinAPITest/CoinAPITest/Program.cs
@@ -1,11 +1,67 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
 using System.Threading;
+using System.Web;
 
 namespace CoinAPITest
 {
     class Program
     {
+        private static string API_KEY = "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c";
+
         static void Main(string[] args)
+        {
+            try
+            {
+                foreach(Crypto crypto in makeAPICall())
+                {
+                    Console.WriteLine(crypto);
+                }
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static List<Crypto> makeAPICall()
+        {
+            var URL = new UriBuilder("https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
+
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString["start"] = "1";
+            queryString["limit"] = "10";
+            queryString["convert"] = "USD";
+
+            URL.Query = queryString.ToString();
+
+            var client = new WebClient();
+            client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
+            client.Headers.Add("Accepts", "application/json");
+            string json = client.DownloadString(URL.ToString());
+
+            dynamic dataSet = JsonConvert.DeserializeObject(json);
+
+            List<Crypto> cryptos = new List<Crypto>();
+
+            foreach(var row in dataSet.data)
+            {
+                Crypto crypto = new Crypto();
+                crypto.id = row.id;
+                crypto.name = row.name;
+                crypto.price = row.quote.USD.price;
+                cryptos.Add(crypto);
+            }
+
+            return cryptos;
+        }
+
+        public void CoinDeskAPI()
         {
             while (true)
             {
